eda7a002fd4451bce714043b97888aed2e2e2685
AbdulHannan08128 <154576645+AbdulHannan08128@users.noreply.github.com>
2024-07-22 15:52:27 +0530

    V1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Landing from "./pages/Landing/Landing";
 
 export default function Home() {
   return (
     <main>
-      
+      <Landing/>
     </main>
   );
 }
